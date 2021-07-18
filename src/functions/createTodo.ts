d9820cb54feb8e3708df78e2c3a277a054164740
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-07-17 22:04:28 -0300

    Create function createTODO

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,4 +1,27 @@
+import { document } from "../utils/dynamodbClient";
+import { v4 as uuidV4 } from "uuid";
+
+interface ICreateTODO {
+  user_id: string;
+  title: string;
+  deadline: Date;
+}
+
 export const handle = async (event) => {
+
+  const { user_id, title, deadline } = JSON.parse(event.body) as ICreateTODO;
+
+  await document.put({
+    TableName: "user_todo",
+    Item: {
+      id: uuidV4(),
+      user_id,
+      title,
+      done: false,
+      deadline,
+    }
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify({
