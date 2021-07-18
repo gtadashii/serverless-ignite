2db03826ef520f867f49eb679f8b0ce18ccd3848
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-07-18 11:39:14 -0300

    Create and return todos from user

--- src/functions/createTodo.ts
+++ src/functions/createTodo.ts
@@ -1,31 +1,35 @@
 import { document } from "../utils/dynamodbClient";
 import { v4 as uuidV4 } from "uuid";
+import { APIGatewayEvent } from "aws-lambda";
 
 interface ICreateTODO {
-  user_id: string;
   title: string;
-  deadline: Date;
+  deadline: string;
 }
 
-export const handle = async (event) => {
+export const handle = async (event: APIGatewayEvent) => {
 
-  const { user_id, title, deadline } = JSON.parse(event.body) as ICreateTODO;
+  const { user_id } = event.pathParameters;
+  const { title, deadline } = JSON.parse(event.body) as ICreateTODO;
+
+  const todo = {
+    id: uuidV4(),
+    user_id,
+    title,
+    done: false,
+    deadline: new Date(deadline).toUTCString(),
+  }
 
   await document.put({
     TableName: "user_todo",
-    Item: {
-      id: uuidV4(),
-      user_id,
-      title,
-      done: false,
-      deadline,
-    }
+    Item: todo,
   }).promise();
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: "Todo created!"
+      message: "Todo created!",
+      todo: todo,
     }),
     headers: {
       "Content-Type": "application/json",
--- src/functions/getTodos.ts
+++ src/functions/getTodos.ts
@@ -1,11 +1,38 @@
-export const handle = async (event) => {
+import { APIGatewayEvent } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+
+export const handle = async (event: APIGatewayEvent) => {
+
+  const { user_id } = event.pathParameters;
+
+  const response = await document.query({
+    TableName: "user_todo",
+    KeyConditionExpression: "user_id = :user_id",
+    ExpressionAttributeValues: {
+      ":user_id": user_id
+    }
+  }).promise();
+
+  const user_todos = response.Items;
+
+  if (user_todos) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(user_todos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: JSON.stringify({
-      message: "Todos listed!"
+      message: "No todo found for this user."
     }),
     headers: {
       "Content-Type": "application/json",
     },
   };
+
 };
